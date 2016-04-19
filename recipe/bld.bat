8737bdb41b95a9d7a135cc6b61d2d0907491d61f
Jake VanderPlas <jakevdp@gmail.com>
2016-04-19 11:16:20 -0700

    fix bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,10 @@
 mkdir build
 cd build
 
-cmake .. ^
-      -G "NMake Makefiles" ^
+cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_BUILD_TYPE=Release ^
-      .
+      ..
 if errorlevel 1 exit 1
 
 nmake
