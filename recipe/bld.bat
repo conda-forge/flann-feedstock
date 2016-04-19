5fc5da4005ab510de5f879702641d696620050c8
Jake VanderPlas <jakevdp@gmail.com>
2016-04-19 12:28:00 -0700

    add tests & build flags

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,14 @@
+:: cannot build flann from within the source directory
 mkdir build
 cd build
 
 cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_BUILD_TYPE=Release ^
+ 	  -D BUILD_MATLAB_BINDINGS=OFF ^
+ 	  -D BUILD_PYTHON_BINDINGS=OFF ^
+ 	  -D BUILD_EXAMPLES=OFF ^
+ 	  -D BUILD_DOC=OFF ^
       ..
 if errorlevel 1 exit 1
 
