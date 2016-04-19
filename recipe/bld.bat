cabb4b169af61b1ead031d104d4ddb08e8b923ae
Jake VanderPlas <jakevdp@gmail.com>
2016-04-19 11:06:27 -0700

    fix windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,15 @@
 mkdir build
 cd build
-cmake ..
-make
+
+cmake .. ^
+      -G "NMake Makefiles" ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      .
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
