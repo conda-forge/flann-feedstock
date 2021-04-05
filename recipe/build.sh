d47fd7341804973e0bcbda8bd0f0ff599bf64039
Tobias Fischer <info@tobiasfischer.info>
2021-04-05 20:40:26 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,13 +4,6 @@
 mkdir build
 cd build
 
-# On OSX, we need to ensure we're using conda's gcc/g++
-if [[ `uname` == Darwin ]]; then
-    export CC=gcc
-    export LD=gcc
-    export CXX=g++
-fi
-
 cmake ${CMAKE_ARGS} .. \
 	 -DCMAKE_INSTALL_PREFIX=$PREFIX \
 	 -DBUILD_MATLAB_BINDINGS:BOOL=OFF \
